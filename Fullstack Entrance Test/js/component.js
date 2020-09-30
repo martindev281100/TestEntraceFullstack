bf2137ec14752d388453178e4004493b7de8b546
martindevox2811 <manhdaokhacmanh@gmail.com>
2020-09-30 19:27:37 +0700

    update commit

---
+++ Fullstack Entrance Test/js/component.js
@@ -0,0 +1,23 @@
+const component = {};
+
+component.quizPage = `
+    <div class="background_image">
+      <div class='quizForm'>
+        <div id="question"></div>
+          <div>Point: <span id="point"></span></div>
+          <button id="restart">Restart</button>
+          <div  class="answer">
+              <button id="answer1"></button>
+          </div>
+          <div  class="answer"><button id="answer2"></button></div>
+          <div  class="answer"> <button  id="answer3"></button> </div>
+          <div  class="answer"><button  id="answer4"></button></div>
+              
+             
+              
+          </div>
+      </div>
+    </div>
+  
+        
+    </div>`;
